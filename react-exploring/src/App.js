687a6a09699d00579870d2f9723fbaadcd9b489b
Shrikant <shri.shirale@gmail.com>
2023-07-27 16:56:40 +0530

    First commit axios

---
+++ react-exploring/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import HttpData from "./components/http-data/HttpData";
+
+function App() {
+  return (
+    <div>
+      <HttpData />
+    </div>
+  );
+}
+
+export default App;
